910c06d8684f647a8c647a8301638191bf3afa05
Amarjeet <kumaramarjeetraj71@gmail.com>
2023-06-02 09:39:51 +0530

    first commit

---
+++ server/utils/users.js
@@ -0,0 +1,39 @@
+[{
+  id: '638fea96a8bcc2d58f0df133',
+  name: 'RSAP4YOU',
+  room: 'Room'
+}]
+
+
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+  
+
+    return users;
+  }
+}
+
+module.exports = {Users};
+
